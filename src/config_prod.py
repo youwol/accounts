c0a57c06f35a864766f4edcad4da16623416e289
Julien DÉCHARNE <jdecharne@youwol.com>
2023-06-09 10:54:29 +0200

    👽️ Update code to py-youwol latest changes

--- src/config_prod.py
+++ src/config_prod.py
@@ -1,13 +1,17 @@
 import os
 
 from youwol.backends.accounts import Configuration
-from youwol.utils import RedisCacheClient, CleanerThread, factory_local_cache
+from youwol.utils import RedisCacheClient
 from youwol.utils.clients.oidc.oidc_config import PrivateClient, OidcInfos
 from youwol.utils.context import DeployedContextReporter
 from youwol.utils.middlewares import AuthMiddleware
 from youwol.utils.middlewares import JwtProviderCookie, JwtProviderBearer
 from youwol.utils.servers.env import REDIS, KEYCLOAK_ADMIN, OPENID_CLIENT, Env
-from youwol.utils.servers.fast_api import AppConfiguration, ServerOptions, FastApiMiddleware
+from youwol.utils.servers.fast_api import (
+    AppConfiguration,
+    ServerOptions,
+    FastApiMiddleware,
+)
 
 
 async def get_configuration():
@@ -20,60 +24,52 @@ async def get_configuration():
     openid_base_url = os.getenv(Env.OPENID_BASE_URL)
     openid_client_id = os.getenv(Env.OPENID_CLIENT_ID)
     openid_client_secret = os.getenv(Env.OPENID_CLIENT_SECRET)
-    openid_infos = OidcInfos(base_uri=openid_base_url,
-                             client=PrivateClient(
-                                 client_id=openid_client_id,
-                                 client_secret=openid_client_secret)
-                             )
+    openid_infos = OidcInfos(
+        base_uri=openid_base_url,
+        client=PrivateClient(
+            client_id=openid_client_id, client_secret=openid_client_secret
+        ),
+    )
     keycloak_admin_base_url = os.getenv(Env.KEYCLOAK_ADMIN_BASE_URL)
     keycloak_admin_client_id = os.getenv(Env.KEYCLOAK_ADMIN_CLIENT_ID)
     keycloak_admin_client_secret = os.getenv(Env.KEYCLOAK_ADMIN_CLIENT_SECRET)
 
     redis_host = os.getenv(Env.REDIS_HOST)
-    jwt_cache = RedisCacheClient(host=redis_host, prefix='jwt_cache')
-
-    cleaner_thread = CleanerThread()
-    pkce_cache = factory_local_cache(cleaner_thread, 'pkce_cache')
-
-    async def on_before_startup():
-        try:
-            cleaner_thread.go()
-        except BaseException as e:
-            print("Error while starting download thread")
-            raise e
+    auth_cache = RedisCacheClient(host=redis_host, prefix="auth_cache")
 
     service_config = Configuration(
-        openid_client=PrivateClient(client_id=openid_client_id, client_secret=openid_client_secret),
+        openid_client=PrivateClient(
+            client_id=openid_client_id, client_secret=openid_client_secret
+        ),
         openid_base_url=openid_base_url,
-        admin_client=PrivateClient(client_id=keycloak_admin_client_id, client_secret=keycloak_admin_client_secret),
+        admin_client=PrivateClient(
+            client_id=keycloak_admin_client_id,
+            client_secret=keycloak_admin_client_secret,
+        ),
         keycloak_admin_base_url=keycloak_admin_base_url,
-        jwt_cache=jwt_cache,
-        pkce_cache=pkce_cache
+        auth_cache=auth_cache,
     )
 
     server_options = ServerOptions(
-        root_path='/api/accounts',
+        root_path="/api/accounts",
         http_port=8080,
         base_path="",
         middlewares=[
             FastApiMiddleware(
-                AuthMiddleware, {
-                    'openid_infos': openid_infos,
-                    'predicate_public_path': lambda url:
-                    url.path.endswith("/healthz") or url.path.startswith("/api/accounts/openid_rp/"),
-                    'jwt_providers': [JwtProviderBearer(),
-                                      JwtProviderCookie(
-                                          jwt_cache=jwt_cache,
-                                          openid_infos=openid_infos
-                                      )],
-                }
+                AuthMiddleware,
+                {
+                    "openid_base_uri": openid_base_url,
+                    "predicate_public_path": lambda url: url.path.endswith("/healthz")
+                    or url.path.startswith("/api/accounts/openid_rp/"),
+                    "jwt_providers": [
+                        JwtProviderBearer(),
+                        JwtProviderCookie(
+                            auth_cache=auth_cache, openid_infos=openid_infos
+                        ),
+                    ],
+                },
             )
-
         ],
         ctx_logger=DeployedContextReporter(),
-        on_before_startup=on_before_startup
     )
-    return AppConfiguration(
-        server=server_options,
-        service=service_config
-    )
+    return AppConfiguration(server=server_options, service=service_config)
